732a80588cc9ad869931e58e17a0d9c88ebeefde
Ruslan Gubaev <gub_ruslan@mail.ru>
2023-03-12 19:44:13 +0300

    changing main class(transfer features in another class)

--- src/main/java/application/ApplicationStart.java
+++ src/main/java/application/ApplicationStart.java
@@ -1,13 +1,10 @@
 package application;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tools.Message;
+import tools.SceneGetter;
 
 public class ApplicationStart extends Application {
 
@@ -21,21 +18,13 @@ public class ApplicationStart extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		Scene scene = getScene("createAccScene");
+		Scene scene = new SceneGetter().getResource("mainScene");
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		primaryStage.show();
+		Message.print("Запуск");
+		System.out.print("Launch");
 
 	}
 
-	// Fast upload fxml files
-	private String fxmlFileFromTemplates(String fileName) throws MalformedURLException {
-		return "/scenes/" + fileName + ".fxml";
-	}
-
-	private Scene getScene(String namePage) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource(fxmlFileFromTemplates(namePage)));
-		Scene scene = new Scene(root, 800, 600);
-		return scene;
-	}
 }
