2d62277a93d3ee3d428da6166c638c1db9484532
Ruslan Gubaev <gub_ruslan@mail.ru>
2023-03-25 19:55:38 +0300

    choice user and go to main scene

--- src/main/java/controllersFXML/ChoiceUserController.java
+++ src/main/java/controllersFXML/ChoiceUserController.java
@@ -1,17 +1,27 @@
 package controllersFXML;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
+import application.ApplicationStart;
 import dataBaseController.BaseController;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import objects.User;
@@ -21,11 +31,19 @@ public class ChoiceUserController extends GridPane implements Initializable{
 
 	@FXML
 	private VBox UsersFields;
+	
+	private int indexCurrentUser;
+	 
 	private BaseController baseController = BaseController.getBaseController();
+	
 	private List<User> users;
+	
+	
 	public void initialize(URL location, ResourceBundle resources) {
 		setUsers(baseController);
 		putUsersInVBOX();
+		initButton();
+		
 	}
 
 	private void setUsers(BaseController baseController) {
@@ -42,13 +60,37 @@ public class ChoiceUserController extends GridPane implements Initializable{
 			try {
 				Parent root = new TemplateGetter().getResource("templateFieldUser");
 				GridPane g = (GridPane)root;
-				Label l =(Label)g.getChildren().get(0);
-				l.setText(user.getUser());
+				Button b =(Button)g.getChildren().get(0);
+				b.setText(user.getUser());
 				UsersFields.getChildren().add(root);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
+	}
+	
+	private void initButton() {
+		for(int i = 0; i < UsersFields.getChildren().size();i++) {
+			//usersButtons.put(((Button)(UsersFields.getChildren().get(i))).getText(),(Button)UsersFields.getChildren().get(i));
+			final Button b = (Button)((GridPane) UsersFields.getChildren().get(i)).getChildren().get(0);
+			b.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					ApplicationStart.setUser(b.getText());
+					try {
+						ApplicationStart.changeScene("mainScene");
+					} catch (MalformedURLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+			});
 		}
 	}
 	
