138738f6b559c3e803482de615e98e561c27a2e4
Ruslan Gubaev <gub_ruslan@mail.ru>
2023-04-08 16:05:04 +0300

    creating event for back button in create acc scene

--- src/main/java/controllersFXML/CreateAccSceneController.java
+++ src/main/java/controllersFXML/CreateAccSceneController.java
@@ -15,12 +15,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ZoomEvent;
 import javafx.scene.layout.GridPane;
+import tools.Message;
 
 
 public class CreateAccSceneController extends GridPane implements Initializable {
 	
 	private InsertUser insertUser = InsertUser.getInsertUser();
+	
+	@FXML
+	private GridPane pane;
 	
 	@FXML
 	private Button createButton;
@@ -31,10 +36,22 @@ public class CreateAccSceneController extends GridPane implements Initializable 
 	@FXML
 	private PasswordField passwordField;
 	
+	@FXML
+	private Button backButton;
 	
 	public void initialize(URL location, ResourceBundle resources) {
 
+		pane.requestFocus();
 		createButton.addEventHandler(MouseEvent.MOUSE_CLICKED, createNewUserEventHandler);
+		backButton.addEventHandler(MouseEvent.MOUSE_CLICKED,backEventHandler);
+		pane.addEventHandler(MouseEvent.MOUSE_CLICKED,new EventHandler<MouseEvent>() {
+
+			public void handle(MouseEvent event) {
+				pane.requestFocus();
+				
+			}
+			
+		});
 	}
 	
 	
@@ -54,6 +71,22 @@ public class CreateAccSceneController extends GridPane implements Initializable 
 		}
 		
 	};
+	private EventHandler<MouseEvent> backEventHandler = new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent event) {
+				try {
+					ApplicationStart.changeScene("mainScene");
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		};
 	
 	
 
